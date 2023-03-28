a22da48ddad04eeca01b4f2d93c0e173c9644cf9
T0MLE <lelaurain.tom@gmail.com>
2023-03-28 14:46:47 +0200

    created pokemon prop

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,20 @@ import PokemonCard from "./components/PokemonCard"
 function App() {
   const [count, setCount] = useState(0)
 
+  const pokemon = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
       <div>
-      <PokemonCard/>
+      <PokemonCard pokemon={pokemon[0]}/>
     </div>
   )
 }
